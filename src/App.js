f2167eaee51af0b835d896811f79ae09927cb0d7
chen.dong <chen_dong85@foxmail.com>
2017-09-01 09:03:29 +0800

    add the react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom'
+import News from  './components/News';
+
 const App = () => {
-  return <div className="test">{'Hello World'}</div>;
+  return <div>
+    <nav>
+      <Link to="/news">News</Link>
+    </nav>
+    <div>
+      <Route path="/news" component={News} />
+    </div>
+  </div>;
 }
 
 export default App;
---
+++ src/components/News.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export  default () => <div>THis is the News</div>
