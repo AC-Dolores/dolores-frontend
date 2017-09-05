cd7428d13bf9dde50f938332e96d7fd7226f608f
chen.dong <chen_dong85@foxmail.com>
2017-09-05 08:41:55 +0800

    add the header css part

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,14 @@ import { Link, Route } from 'react-router-dom';
 import './Header.scss';
 
 export  default () => <header>
-  <nav className="Header">
-    <Link to="/topics" className="Link">热门话题</Link>
-    <Link to="/news">科技动态</Link>
-    <Link to="/news">开发者动态</Link>
-  </nav>
+  <div className="header">
+    <nav className="nav-bar">
+      <a className="logo">AaronIsMe</a>
+      <div className="link-box">
+        <Link to="/topics" className="link">热门话题</Link>
+        <Link to="/news" className="link">科技动态</Link>
+        <Link to="/news" className="link" >开发者动态</Link>
+      </div>
+    </nav>
+  </div>
 </header>
