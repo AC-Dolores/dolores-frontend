dd6b9c268e584af895ca2611a30ed6e8d2790eed
chen.dong <chen_dong85@foxmail.com>
2017-09-20 08:14:47 +0800

    add the scroll topic

--- src/components/Topic/Topics.js
+++ src/components/Topic/Topics.js
@@ -8,12 +8,38 @@ class Topics extends Component {
   constructor(props){
     super(props);
     this.state = {
-      topics: []
-    }
+      topics: [],
+      lastCursor: 0
+    };
+    this.handleScroll = this.handleScroll.bind(this);
   }
   
   componentDidMount(){
-    getTopics().then(topics => this.setState({ topics }))
+    window.addEventListener('scroll', this.handleScroll);
+    this.fetchData()
+  }
+  
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  
+  handleScroll(){
+    const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+    const windowBottom = windowHeight + window.pageYOffset;
+    if(windowBottom >= docHeight) {
+      this.fetchData()
+    }
+  }
+  
+  fetchData(){
+    getTopics(this.state.lastCursor).then(topics => {
+      const oldTopics = this.state.topics;
+      const lastCursor = topics[topics.length - 1].order;
+      this.setState({topics:[...oldTopics, ...topics], lastCursor})
+    })
   }
   
   render(){
